d8e7a31f6e8bf9e01114cb9e71f3a9dc13d17eb1
meeting77smile <2054865827@qq.com>
2025-03-03 12:34:29 +0800

    新增事项规划功能

--- frontEnd/JiYu/JiYuLearning/login/login.js
+++ frontEnd/JiYu/JiYuLearning/login/login.js
@@ -1,70 +1,52 @@
-import {loginUser, signupUser} from '../index/networkUtils.js';
+//点击"登录"按钮之后，实现登录功能：
+function btn_login() {
 
-let signup = document.querySelector('#signup');
-let signin = document.querySelector('#signin');
-let body = document.querySelector('body');
+    //手机端适配：（点击登录直接跳转)
+    window.location.href = '../index/chat1.html';
+    // 获取用户名和密码
+    // const username = document.getElementById('username').value;
+    // localStorage.setItem('username', username);
+    // const password = document.getElementById('password').value;
 
-signup.onclick = function(){
-    console.log("____跳转到注册页面_____");
-    body.classList.add('signup');
+    // // 创建一个 User 对象
+    // const user = {
+    //     username: username,
+    //     password: password
+    // };
+
+    // // 发送登录请求
+    // fetch('http://localhost:8080/login', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(user)
+    // })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         if (data.code === 1) {
+    //             // 登录成功，保存登录状态信息
+    //             localStorage.setItem('isLoggedIn', 'true');
+    //             // 保存账号名localStorage
+    //             localStorage.setItem('username', username);
+    //             alert('登录成功');
+    //             // 可以跳转到其他页面
+    //             window.location.href = '../index/chat1.html';
+    //         } else {
+    //             if(data.msg == "error username"){
+    //                 alert("用户名输入错误");
+    //             }
+    //             else{
+    //                 alert("密码输入错误")
+    //             }
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.error('登录请求出错:', error);
+    //     });
 }
-signin.onclick = function(){
-    console.log("____跳转到登录页面_____");
-    body.classList.remove('signup');
-}
 
-document.querySelector('#signinForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // 阻止表单默认提交行为
-
-    const username = document.querySelector('#signinUsername').value;
-    const password = document.querySelector('#signinPassword').value;
-
-    window.location.href = "../index/chat1.html";
-    alert('登录成功！')
-    loginUser(username, password).then((token) => {
-        console.log("登录成功，跳转到index.html");
-        alert('登录成功！')
-        // 保存 用户名 用户ID 到localStorage
-        localStorage.setItem('username', username);
-        localStorage.setItem('userId', token);
-        // 跳转到chat1.html页面
-        window.location.href = "../index/chat1.html";
-    }).catch(error => {
-        console.error("登录失败：", error);
-        alert('登录失败！')
-    });
-});
-
-// 绑定注册表单提交事件
-document.querySelector('#signupForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // 阻止表单默认提交行为
-
-    // 获取用户名、密码和确认密码
-    const username = document.querySelector('#signupUsername').value;
-    const password = document.querySelector('#signupPassword').value;
-    const confirmPassword = document.querySelector('#confirmPassword').value;
-
-    window.location.href = "login.html"; // 跳转到登录页面
-
-    // 调用signupUser进行注册
-    signupUser(username, password, confirmPassword).then(() => {
-        console.log("注册成功，跳转到登录页面");
-        window.location.href = "login.html"; // 跳转到登录页面
-    }).catch(error => {
-        console.error("注册失败：", error);
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    // 等待移动动画结束后开始呼吸动画
-    setTimeout(function() {
-        document.querySelector('.intro-overlay p').style.transform = 'translateY(10px)';
-        document.body.classList.add('breathing');
-    }, 1000);
-
-    document.querySelector('.intro-overlay').addEventListener('click', function() {
-        document.body.classList.add('clear-bg'); // 清除模糊效果
-        document.querySelector('.intro-overlay').style.display = 'none'; // 隐藏intro-overlay
-        document.querySelector('.container').style.display = 'flex'; // 显示登录框
-    });
-});
+// //点击登录后跳转至注册页面
+// function goToRegisterPage() {
+//     window.location.href = './register.html';
+// }
